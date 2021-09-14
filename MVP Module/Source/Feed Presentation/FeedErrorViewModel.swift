e31ea80c7720fc7f19c252f5ac5e5947d08c5649
Anton <by.ios.anton@yandex.by>
2021-09-14 13:32:13 +0300

    added ability to show error in mvp solution

---
+++ MVP Module/Source/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedErrorViewModel.swift
+//  MVP
+//
+//  Created by Антон on 14.09.21.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+struct FeedErrorViewModel {
+	var error: String?
+}
--- MVP Module/Source/Feed UI/Controllers/FeedViewController.swift
+++ MVP Module/Source/Feed UI/Controllers/FeedViewController.swift
@@ -8,7 +8,7 @@ protocol FeedViewControllerDelegate {
 	func didRequestFeedRefresh()
 }
 
-public final class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching, FeedLoadingView {
+public final class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching, FeedLoadingView, FeedErrorView {
 	@IBOutlet private(set) public var errorView: ErrorView?
 
 	var delegate: FeedViewControllerDelegate?
@@ -37,6 +37,14 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
 
 	func display(_ cellControllers: [FeedImageCellController]) {
 		tableModel = cellControllers
+	}
+
+	func display(_ viewModel: FeedErrorViewModel) {
+		if let error = viewModel.error {
+			errorView?.show(message: error)
+		} else {
+			errorView?.hideMessage()
+		}
 	}
 
 	public override func viewDidLayoutSubviews() {
